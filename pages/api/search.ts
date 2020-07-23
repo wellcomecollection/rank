544e9f333dee137eb0791804e9be49c9232d9555
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-07-23 15:28:42 +0100

    add basic search endpoint

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -3,11 +3,21 @@ import rankEval from "../../rank_eval.json";
 import searchTemplates from "../../search_templates.json";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { query } = req.query;
   const { ES_USER, ES_PASSWORD, ES_URL } = process.env;
-  const rankEvalEnpoint = `https://${ES_URL}/works-20200717/_rank_eval`;
+  const rankEvalEnpoint = `https://${ES_URL}/works-20200717/_search/template`;
+  const searchTemplate = searchTemplates.find(
+    (template) => template.id === "multi_match"
+  );
+
   const resp = await fetch(rankEvalEnpoint, {
     method: "POST",
-    body: JSON.stringify({ ...rankEval, templates: searchTemplates }),
+    body: JSON.stringify({
+      source: searchTemplate.template.source,
+      params: {
+        query,
+      },
+    }),
     headers: {
       Authorization: `Basic ${Buffer.from(`${ES_USER}:${ES_PASSWORD}`).toString(
         "base64"
@@ -17,7 +27,14 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   });
   const json = await resp.json();
 
+  const worksResponse = json.hits.hits.map((hit) => {
+    return {
+      id: hit._id,
+      title: hit._source.data.title,
+    };
+  });
+
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(json));
+  res.end(JSON.stringify(worksResponse));
 };
