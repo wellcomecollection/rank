34b5fcb42038df0ab0232b3b89e7799fa837c5b7
James Gorrie <j.gorrie@wellcome.ac.uk>
2021-03-23 15:32:11 +0000

    add test queries to search

--- pages/api/search/works.ts
+++ pages/api/search/works.ts
@@ -1,10 +1,35 @@
 import { Client } from "@elastic/elasticsearch";
 import { NextApiRequest, NextApiResponse } from "next";
-import { getSearchTemplates } from "../../../services/search-templates";
+import {
+  getSearchTemplates,
+  Template,
+} from "../../../services/search-templates";
 
 const { ES_USER, ES_PASSWORD, ES_CLOUD_ID } = process.env;
 
+async function getCurrentQuery(): Promise<Template> {
+  const searchTemplates = await getSearchTemplates("prod");
+  const template = searchTemplates.templates.find((template) =>
+    template.index.startsWith("works")
+  );
+
+  return template;
+}
+async function getTestQuery(id: string): Promise<Template> {
+  const currentTemplate = await getCurrentQuery();
+  const index = currentTemplate.index;
+  const query = await import(`../../../queries/${id}`).then((q) => q.default);
+
+  return {
+    id,
+    index,
+    template: { source: { query: query } },
+  };
+}
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { query, queryId } = req.query;
+
   const client = new Client({
     cloud: {
       id: ES_CLOUD_ID,
@@ -14,11 +39,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       password: ES_PASSWORD,
     },
   });
-  const searchTemplates = await getSearchTemplates("prod");
-  const template = searchTemplates.templates.find((template) =>
-    template.index.startsWith("works")
-  );
-  const { query } = req.query;
+  const template = queryId
+    ? await getTestQuery(queryId.toString())
+    : await getCurrentQuery();
+
   const resp = await client.searchTemplate({
     index: template.index,
     body: {
---
+++ queries/languages.ts
@@ -0,0 +1,80 @@
+const query = {
+  bool: {
+    should: [
+      {
+        prefix: {
+          "data.title.keyword": {
+            value: "{{query}}",
+            boost: 1000,
+          },
+        },
+      },
+      {
+        multi_match: {
+          query: "{{query}}",
+          fields: [
+            "state.canonicalId^1000.0",
+            "state.sourceIdentifier.value^1000.0",
+            "data.otherIdentifiers.value^1000.0",
+            "data.items.id.canonicalId^1000.0",
+            "data.items.id.sourceIdentifier.value^1000.0",
+            "data.items.id.otherIdentifiers.value^1000.0",
+            "data.imageData.id.canonicalId^1000.0",
+            "data.imageData.id.sourceIdentifier.value^1000.0",
+            "data.imageData.id.otherIdentifiers.value^1000.0",
+          ],
+          type: "best_fields",
+          analyzer: "whitespace_analyzer",
+          operator: "Or",
+        },
+      },
+      {
+        multi_match: {
+          query: "{{query}}",
+          fields: [
+            "data.title^100.0",
+            "data.title.english^100.0",
+            "data.title.shingles^100.0",
+            "data.alternativeTitles^100.0",
+            "data.lettering",
+          ],
+          type: "best_fields",
+          fuzziness: "AUTO",
+          operator: "And",
+        },
+      },
+      {
+        multi_match: {
+          query: "{{query}}",
+          fields: [
+            "data.contributors.agent.label^1000.0",
+            "data.subjects.concepts.label^10.0",
+            "data.genres.concepts.label^10.0",
+            "data.production.*.label^10.0",
+            "data.description",
+            "data.physicalDescription",
+            "data.language.label",
+            "data.edition",
+            "data.notes.content",
+            "data.collectionPath.path",
+            "data.collectionPath.label",
+          ],
+          type: "cross_fields",
+          operator: "And",
+        },
+      },
+    ],
+    filter: [
+      {
+        term: {
+          type: {
+            value: "Visible",
+          },
+        },
+      },
+    ],
+    minimum_should_match: "1",
+  },
+};
+
+export default query;
