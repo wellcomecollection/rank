dc03efa64bff1d6e3c7e2e6d2278c3e7f25c2450
James Gorrie <j.gorrie@wellcome.org>
2021-05-06 10:53:49 +0100

    slightly nicer looking form

--- components/QueryForm.tsx
+++ components/QueryForm.tsx
@@ -16,48 +16,53 @@ const QueryForm = (props: Props) => {
   }, [props.query])
 
   return (
-    <form className="mb-5">
-      <label className="p-2 mr-10 inline-block border-2 border-purple-400 rounded-full">
-        Query:
-        <input
-          className="ml-2"
-          type="text"
-          name="query"
-          value={query}
-          onChange={(event) => setQuery(event.currentTarget.value)}
-        />
-      </label>
-      <label className="p-2 mr-5 inline-block border-2 border-purple-400 rounded-full">
-        Endpoint
-        <select
-          className="ml-2"
-          name="endpoint"
-          onChange={(event) => setEndpoint(event.currentTarget.value)}
-          value={endpoint}
+    <form className="mb-5 border-2 border-purple-400 rounded-full flex">
+      <div className="flex-grow">
+        <label className="p-2 mr-4 inline-block">
+          Query
+          <input
+            className="ml-2"
+            type="text"
+            name="query"
+            value={query}
+            placeholder="What are you looking for?"
+            onChange={(event) => setQuery(event.currentTarget.value)}
+          />
+        </label>
+        <label className="p-2 mr-4 inline-block">
+          Endpoint
+          <select
+            className="ml-2"
+            name="endpoint"
+            onChange={(event) => setEndpoint(event.currentTarget.value)}
+            value={endpoint}
+          >
+            <option value="works">works</option>
+            <option value="images">images</option>
+          </select>
+        </label>
+      </div>
+      <div className="flex-shrink">
+        <label className="p-2 inline-block">
+          🧪 <span className="sr-only">Test query</span>
+          <input
+            type="checkbox"
+            name="useTestQuery"
+            value="true"
+            checked={useTestQuery || false}
+            onChange={(event) => {
+              setUseTestQuery(event.currentTarget.checked ? true : undefined)
+            }}
+          />
+        </label>
+        <button
+          className="p-2 ml-3"
+          aria-label="Search catalogue"
+          type="submit"
         >
-          <option value="works">works</option>
-          <option value="images">images</option>
-        </select>
-      </label>
-      <label className="p-2 mr-5 inline-block border-2 border-purple-400 rounded-full">
-        🧪 <span className="sr-only">Test query</span>
-        <input
-          type="checkbox"
-          name="useTestQuery"
-          value="true"
-          checked={useTestQuery || false}
-          onChange={(event) => {
-            setUseTestQuery(event.currentTarget.checked ? true : undefined)
-          }}
-        />
-      </label>
-      <button
-        className="p-2 ml-3 mr-5 inline-block border-2 border-purple-400 rounded-full"
-        aria-label="Search catalogue"
-        type="submit"
-      >
-        🔎
-      </button>
+          🔎
+        </button>
+      </div>
     </form>
   )
 }
