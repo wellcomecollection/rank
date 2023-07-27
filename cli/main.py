23aa2cf998b7598c78cfb1ec19a670ca3006c435
jamie <j.parkinson@wellcome.org>
2023-07-27 15:45:05 +0100

    A CLI called rank

---
+++ cli/main.py
@@ -0,0 +1,14 @@
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def hello(name: str):
+    print(f"Hello {name}")
+
+# This is here temporarily while there is only one command
+# https://typer.tiangolo.com/tutorial/commands/one-or-multiple/#one-command-and-one-callback
+@app.callback()
+def callback():
+    pass
