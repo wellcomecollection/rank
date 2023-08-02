8460694cbb92d6f7062f4a68864d0b882848ae25
harrisonpim <hello@harrisonpim.com>
2023-08-02 11:53:37 +0100

    add flags to the cli to filter tests by content type, module, or id

--- cli/main.py
+++ cli/main.py
@@ -1,5 +1,8 @@
 import pytest
 import typer
+from typing_extensions import Annotated
+
+from . import ContentType, root_test_directory
 
 app = typer.Typer(
     name="rank",
@@ -14,7 +17,36 @@ def hello(name: str):
     print(f"Hello {name}")
 
 
-@app.command()
-def test():
-    return_code = pytest.main(["cli/relevance_tests/"])
+@app.command(help="Run the relevance tests")
+def test(
+    test_id: Annotated[
+        str,
+        typer.Option(
+            "--id",
+            "--group",
+            "-g",
+            help="The ID of an individual test (or group of tests) to run.",
+            case_sensitive=False,
+        ),
+    ] = None,
+    content_type: Annotated[
+        ContentType,
+        typer.Option(
+            "--content-type",
+            "-c",
+            help="The content type to run tests for",
+            case_sensitive=False,
+            show_choices=True,
+        ),
+    ] = None,
+):
+    test_directory = root_test_directory
+    if content_type:
+        test_directory = test_directory / content_type
+
+    if test_id:
+        return_code = pytest.main([test_directory, "-k", test_id])
+    else:
+        return_code = pytest.main([test_directory])
+
     raise typer.Exit(code=return_code)
