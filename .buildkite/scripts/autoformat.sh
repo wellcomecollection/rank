3b4a5b6a3a4308163cb3e23ba5fe677d076b3f57
harrisonpim <hello@harrisonpim.com>
2023-08-02 12:43:59 +0100

    only commit changes from scripts if we're in a buildkite environment

--- .buildkite/scripts/autoformat.sh
+++ .buildkite/scripts/autoformat.sh
@@ -9,21 +9,24 @@ ROOT=$(git rev-parse --show-toplevel)
 black $ROOT
 isort $ROOT/**/*.py
 
-# Commit any changes
-if [[ `git status --porcelain` ]]; then
-  git config user.name "Buildkite on behalf of Wellcome Collection"
-  git config user.email "wellcomedigitalplatform@wellcome.ac.uk"
+# Only run this bit if we're in buildkite
+if [[ "${BUILDKITE:-}" ]]; then
+    # Commit any changes
+    if [[ `git status --porcelain` ]]; then
+        git config user.name "Buildkite on behalf of Wellcome Collection"
+        git config user.email "wellcomedigitalplatform@wellcome.ac.uk"
 
-  git remote add ssh-origin $BUILDKITE_REPO || true
-  git fetch ssh-origin
-  git checkout --track ssh-origin/$BUILDKITE_BRANCH || true
+        git remote add ssh-origin $BUILDKITE_REPO || true
+        git fetch ssh-origin
+        git checkout --track ssh-origin/$BUILDKITE_BRANCH || true
 
-  git add --verbose --update
-  git commit -m "Apply auto-formatting rules"
+        git add --verbose --update
+        git commit -m "Apply auto-formatting rules"
 
-  git push ssh-origin HEAD:$BUILDKITE_BRANCH
-  exit 1;
-else
-  echo "There were no changes from auto-formatting"
-  exit 0;
+        git push ssh-origin HEAD:$BUILDKITE_BRANCH
+        exit 1;
+    else
+        echo "There were no changes from auto-formatting"
+        exit 0;
+    fi
 fi
