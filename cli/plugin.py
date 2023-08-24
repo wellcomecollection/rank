666967543f346f70c6cd2b93e4649d070bf786b5
jamie <j.parkinson@wellcome.org>
2023-08-24 10:43:48 +0100

    Stop printing the full path

--- cli/plugin.py
+++ cli/plugin.py
@@ -1,4 +1,5 @@
 import json
+import os
 import re
 
 import boto3
@@ -64,11 +65,45 @@ class SearchUnderTest:
         self.images_index = search_templates["images"]["index"]
 
 
+
+
 class RankPlugin:
     def __init__(self, *, context: typer.Context):
         self.role_arn = context.meta["role_arn"]
         self.catalogue_api_url = context.meta["catalogue_api_url"]
         self.sut = SearchUnderTest(self.catalogue_api_url)
+
+    # This is a hack to rewrite test names in the output, excluding their
+    # common prefix. This is useful in particular for when rank is installed
+    # as a package (ie in the Docker images), where without this hack every
+    # test name is prefixed with the `site-packages` location
+    common_path_key = pytest.StashKey[str]()
+
+    @pytest.hookimpl()
+    def pytest_configure(self, config):
+        terminal = config.pluginmanager.getplugin('terminal')
+
+        class ShortPathReporter(terminal.TerminalReporter):
+            # This modifies the implementation at
+            # https://github.com/pytest-dev/pytest/blob/7.4.0/src/_pytest/terminal.py#L426
+            def write_fspath_result(self, nodeid: str, res, **markup: bool) -> None:
+                fspath = self.config.rootpath / nodeid.split("::")[0]
+                if self.currentfspath is None or fspath != self.currentfspath:
+                    if self.currentfspath is not None and self._show_progress_info:
+                        self._write_progress_information_filling_space()
+                    self.currentfspath = fspath
+                    commonpath = self.config.stash[RankPlugin.common_path_key]
+                    shortpath = str(fspath.relative_to(commonpath))
+                    self._tw.line()
+                    self._tw.write(shortpath + " ")
+                self._tw.write(res, flush=True, **markup)
+
+        terminal.TerminalReporter = ShortPathReporter
+
+    @pytest.hookimpl(tryfirst=True)
+    def pytest_collection_modifyitems(self, session, config, items):
+        commonpath = os.path.commonpath([item.path for item in items])
+        config.stash[RankPlugin.common_path_key] = commonpath
 
     @pytest.fixture(scope="session")
     def aws_session(self) -> boto3.session.Session:
