9d48bf8fc6d26057b8867811929a34f0940c0270
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2023-11-09 12:47:42 +0000

    Apply auto-formatting rules

--- cli/relevance_tests/works/test_precision.py
+++ cli/relevance_tests/works/test_precision.py
@@ -144,8 +144,8 @@ test_cases = [
     PrecisionTestCase(
         search_terms="The Secrets of Alchemy",
         expected_ids=["rtdee482"],
-        description="Case-insensitive partial titles"
-    )
+        description="Case-insensitive partial titles",
+    ),
 ]
 
 
